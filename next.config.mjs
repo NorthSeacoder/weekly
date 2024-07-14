7fc5d29816f8f63c506299cdc0825c6639b84b4e
mengpeng <pengmeng@yangqianguan.com>
2024-07-14 15:51:08 +0800

    chore: add sentry

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,55 @@
-import createMDX from "@next/mdx";
-import remarkGfm from "remark-gfm";
+import {withSentryConfig} from '@sentry/nextjs';
+import createMDX from '@next/mdx';
+import remarkGfm from 'remark-gfm';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configure `pageExtensions`` to include MDX files
-  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
-  // Optionally, add any other Next.js config below
-  output:'export'
+    // Configure `pageExtensions`` to include MDX files
+    pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+    // Optionally, add any other Next.js config below
+    output: 'export'
 };
 
 const withMDX = createMDX({
-  // Add markdown plugins here, as desired
-  options: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [],
-  },
+    // Add markdown plugins here, as desired
+    options: {
+        remarkPlugins: [remarkGfm],
+        rehypePlugins: []
+    }
 });
 
 // Wrap MDX and Next.js config with each other
-export default withMDX(nextConfig);
+export default withSentryConfig(withMDX(nextConfig), {
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
+
+    org: 'nsea',
+    project: 'weekly',
+
+    // Only print logs for uploading source maps in CI
+    silent: !process.env.CI,
+
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
+
+    // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+    // This can increase your server load as well as your hosting bill.
+    // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+    // side errors will fail.
+    tunnelRoute: '/monitoring',
+
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+
+    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+    // See the following for more information:
+    // https://docs.sentry.io/product/crons/
+    // https://vercel.com/docs/cron-jobs
+    automaticVercelMonitors: true
+});
