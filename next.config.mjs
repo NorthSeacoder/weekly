4782d45651e5725ac16c9e0438a5dea601717585
mengpeng <pengmeng@yangqianguan.com>
2024-07-14 21:55:25 +0800

    fix: 关闭 sentry tunnelRoute

--- next.config.mjs
+++ next.config.mjs
@@ -35,11 +35,11 @@ export default withSentryConfig(withMDX(nextConfig), {
     // Upload a larger set of source maps for prettier stack traces (increases build time)
     widenClientFileUpload: true,
 
-    // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-    // This can increase your server load as well as your hosting bill.
-    // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-    // side errors will fail.
-    tunnelRoute: '/monitoring',
+    // // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+    // // This can increase your server load as well as your hosting bill.
+    // // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+    // // side errors will fail.
+    // tunnelRoute: '/monitoring',
 
     // Hides source maps from generated client bundles
     hideSourceMaps: true,
