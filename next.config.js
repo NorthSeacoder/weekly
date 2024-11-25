be92947c79087d35f058d42ea2e0501f01e26546
mengpeng <pengmeng@yangqianguan.com>
2024-11-25 19:35:28 +0800

    fix: 尝试修复 build 报错

--- next.config.js
+++ next.config.js
@@ -4,12 +4,11 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // 添加排除配置
   webpack: (config, { isServer }) => {
     // 排除 extension 目录
     config.watchOptions = {
       ...config.watchOptions,
-      ignored: [...(config.watchOptions?.ignored || []), '**/extension/**']
+      ignored: /extension\/.*/
     };
     return config;
   }
