dd1f5e99555743763171bafff2ad1aa8bf7dd71f
mengpeng <pengmeng@yangqianguan.com>
2025-02-23 20:52:49 +0800

    feat: 接入 sentry

--- astro.config.ts
+++ astro.config.ts
@@ -4,7 +4,8 @@ import weekly from './integration';
 import icon from 'astro-icon';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
-import { readingTimeRemarkPlugin } from './src/utils/frontmatter';
+import sentry from '@sentry/astro';
+// import {readingTimeRemarkPlugin} from './src/utils/frontmatter';
 
 export default defineConfig({
     output: 'static',
@@ -17,17 +18,26 @@ export default defineConfig({
         mdx(),
         icon({
             include: {
-                tabler: ['*'],
+                tabler: ['*']
             }
         }),
-        weekly()
+        weekly(),
+        sentry({
+            dsn: process.env.SENTRY_DSN, //
+            tracesSampleRate: 1.0, // 跟踪采样率，1.0 表示捕获所有请求
+            // 可选：添加浏览器跟踪集成
+            sourceMapsUploadOptions: {
+                project: 'weekly', // 在 Sentry 中设置的项目名称
+                authToken: process.env.NEXT_PUBLIC_SENTRY_AUTH_TOKEN // 用于上传 source maps 的 token
+            }
+        })
     ],
     markdown: {
         // remarkPlugins: [readingTimeRemarkPlugin]
     },
     vite: {
         define: {
-          'import.meta.env.BUILD_TIME': JSON.stringify(new Date().toISOString())
+            'import.meta.env.BUILD_TIME': JSON.stringify(new Date().toISOString())
         }
-      }
+    }
 });
---
+++ sentry.client.config.js
@@ -0,0 +1,21 @@
+// sentry.client.config.js
+import * as Sentry from '@sentry/astro';
+
+Sentry.init({
+    tracesSampleRate: 1.0, // 性能跟踪采样率
+    replaysSessionSampleRate: 0.1, // 10% 的会话回放
+    replaysOnErrorSampleRate: 1.0, // 错误时 100% 回放
+    integrations: [
+        Sentry.browserTracingIntegration(), // 浏览器性能跟踪
+        Sentry.replayIntegration({
+            // 会话回放
+            maskAllText: false,
+            blockAllMedia: false
+        }),
+        Sentry.breadcrumbsIntegration({
+            console: true, // 捕获 console.log
+            dom: true, // 捕获 DOM 事件（如点击）
+        }),
+        Sentry.dedupeIntegration(), // 去重
+    ]
+});
